b641c42878b0a3c1a5de752017947d334bd88dd1
Alex Comin <alexandr.komin91@gmail.com>
2016-08-07 03:50:46 +0300

    Переделал с использованием библиотеки lxml

--- get_img_status.py
+++ get_img_status.py
@@ -1,5 +1,6 @@
-import os
+import requests
 import re
+from lxml.html import parse
 from time import sleep
 from datetime import datetime
 
@@ -13,63 +14,60 @@ else:
     url = '.'.join(url)
     
 host = "/".join(url.split('/')[:-1])
-get_xml = os.popen('curl '+url).read()
-array_xml = get_xml.split('\n')
 url_xml = []
 url_img_bad = []
 url_img = {}
 counter = 0
 
-for i in array_xml:
-    i = i.split(" ")
-    a = list(filter(bool, map(str.rstrip, i)))
-    if re.match('<loc>', a[0]):
-        result_url = a[0].replace('<loc>','').replace('</loc>','')
-        url_xml.append(result_url)
+get_xml = parse(url).getroot()
+links_obj = get_xml.cssselect("loc")
+
+for link in links_obj:
+        url_xml.append(link.text)
 
 
-def test(arg):
-    get_page = os.popen('curl '+arg).read()
-    array_page = get_page.split('\n')
-    for i in array_page:
-        if re.search('<img', i):
-            i = i.split(' ')
-            for index in i:
-                if re.search('src=', index):
-                    new_string = index.replace('"','').replace('src=','')
-                    image_format = new_string.split('.')
-                    if image_format[len(image_format) -1] in ['jpg', 'jpeg', 'png', 'svg', 'gif']:
-                        if not re.match('http', new_string):
-                            if host+new_string not in url_img:
-                                url_img[host+new_string] = [arg]
-                            else:
-                                url_img[host+new_string].append(arg)
-                        else:
-                            if new_string not in url_img:
-                                url_img[new_string] = [arg]
-                            else:
-                                url_img[new_string].append(arg)
+def parse_page(url):
+    get_page = parse(url).getroot()
+    image_obj = get_page.cssselect("img")
+    for image in image_obj:
+        new_string = image.get('src')
+        image_format = image.get('src').split('.')
+        if image_format[len(image_format) -1] in ['jpg', 'jpeg', 'png', 'svg', 'gif']:
+            if not re.match('http', new_string):
+                if host+new_string not in url_img:
+                    url_img[host+new_string] = [url]
+                else:
+                    url_img[host+new_string].append(url)
+            else:
+                if new_string not in url_img:
+                    url_img[new_string] = [url]
+                else:
+                    url_img[new_string].append(url)
                                 
-    
-for i in url_xml:
+for url in url_xml:
     sleep(0.2)
-    test(i)
+    parse_page(url)
     counter += 1
     print("Страниц просканировано", counter, "из", len(url_xml))
     
 print('\nПодождите, идет проверка всех найденых изображений. Это может занять некоторое время')
 
 for img in url_img:
-    response = os.popen('curl -Is \''+img+'\' | head -1').read().split(' ')
-    if len(response) < 2:
-        print('Что то не так с изображением, проверьте страницы', img, url_img[img])
-        continue
-    else: 
-        if response[1] != '200' and response[1] != '301':
-            print(img, response[1])
+    try:
+        response = requests.get(img).status_code
+    except requests.ConnectionError:
+        response = 0
+    if response == 0:
+        print(img, 'Не удается получить доступ к сайту')
+        for page in url_img[img]:
+            if page not in url_img_bad:
+                url_img_bad.append(page)
+    else:
+        if response != 200 and response != 301:
+            print(img, response)
             for page in url_img[img]:
                 if page not in url_img_bad:
                     url_img_bad.append(page)
 
 time_now = datetime.strftime(datetime.now(), "%d/%m/%y %H:%M:%S");
-print('\nСтраницы\n'+'\n'.join(url_img_bad), "\n"+time_start+"\n"+time_now)
+print('\nНайденные страницы\n'+'\n'.join(url_img_bad), "\nНачало\n"+time_start+"\nКонец\n"+time_now)
